9d9cf464ab7dcdd60bb41746c7a950d751121c13
Julik11 <lisinnikita777@gmail.com>
2025-04-06 00:00:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,5 @@
 // Copyright 2021 NNTU-CS
+#include <algorithm>
 
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
@@ -14,29 +15,29 @@ int countPairs1(int *arr, int len, int value) {
 
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
-  int left = 0, right = len - 1;
+  int left = 0;
+  int right = len - 1;
+
   while (left < right) {
     int sum = arr[left] + arr[right];
-    if (sum == value) {
-      int l_val = arr[left], r_val = arr[right];
-      int l_count = 1, r_count = 1;
 
-      while (left + 1 < right && arr[left + 1] == l_val) {
-        l_count++;
+    if (sum == value) {
+      int leftVal = arr[left];
+      int rightVal = arr[right];
+
+      int leftCount = 0;
+      while (left < right && arr[left] == leftVal) {
         left++;
+        leftCount++;
       }
-      while (right - 1 > left && arr[right - 1] == r_val) {
-        r_count++;
+
+      int rightCount = 0;
+      while (right >= left && arr[right] == rightVal) {
         right--;
+        rightCount++;
       }
-      if (l_val == r_val) {
-        count += (l_count * (l_count - 1)) / 2;
-      } else {
-        count += l_count * r_count;
-      }
-      
-      left++;
-      right--;
+
+      count += leftCount * rightCount;
     } else if (sum < value) {
       left++;
     } else {
@@ -50,23 +51,15 @@ int countPairs3(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
     int complement = value - arr[i];
-    if (complement < 0) continue;
-    int left = i + 1;
-    int right = len - 1;
-    
-    while (left <= right) {
-      int mid = (left + right) / 2;
-      if (arr[mid] < complement) left = mid + 1;
-      else right = mid - 1;
-    }
-    
-    while (left < len && arr[left] == complement) {
-      count++;
-      left++;
-    }
+
+    auto lower = std::lower_bound(arr + i + 1, arr + len, complement);
+    auto upper = std::upper_bound(arr + i + 1, arr + len, complement);
+
+    count += upper - lower;
   }
   return count;
 }
+
 
 
 
